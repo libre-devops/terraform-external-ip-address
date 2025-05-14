921fee5285f4d490d270ab6ca4992500cfb5620b
Craig Thacker <craigthackerx@gmail.com>
2025-05-14 13:20:25 +0100

    Update code

--- variables.tf
+++ variables.tf
@@ -1,19 +1,5 @@
-variable "location" {
-  description = "The location for this resource to be put in"
+variable "working_dir" {
   type        = string
-}
-
-variable "name" {
-  type        = string
-  description = "The name of the VNet gateway"
-}
-
-variable "rg_name" {
-  description = "The name of the resource group, this module does not create a resource group, it is expecting the value of a resource group already exists"
-  type        = string
-}
-
-variable "tags" {
-  type        = map(string)
-  description = "A map of the tags to use on the resources that are deployed with this module."
+  description = "The working directory for the module"
+  default     = null
 }
